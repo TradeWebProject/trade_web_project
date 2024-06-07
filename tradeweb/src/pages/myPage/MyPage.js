0a6a4b019f251794a5db8da3da76776e55e9cf89
choimeeyoung <choimeeyoung94@gmail.com>
2024-06-08 04:56:52 +0900

    찜목록에 페이징 UI 틀 적용

--- tradeweb/src/pages/myPage/MyPage.js
+++ tradeweb/src/pages/myPage/MyPage.js
@@ -278,6 +278,16 @@ const MyPage = () => {
                                     </SearchItem>
                                 ))}
                             </SearchResultList>
+                            <div class="pagination">
+                                <a href="#">&laquo;</a>
+                                <a href="#">1</a>
+                                <a href="#">2</a>
+                                <a href="#">3</a>
+                                <a href="#">4</a>
+                                <a href="#">5</a>
+                                <a href="#">6</a>
+                                <a href="#">&raquo;</a>
+                            </div>
                         </Container>
                         
                     </Box>
